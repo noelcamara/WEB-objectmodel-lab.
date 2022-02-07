81bc5341253d42cfbfd687308186ace8a7886b6b
noel <noel.camara@etu.univ-lehavre.fr>
2022-02-07 22:12:58 +0100

    Ajout de la class TimeSeries

--- src/index.js
+++ src/index.js
@@ -3,4 +3,11 @@ export const version = () => '1.0.0';
 /* le modèle objet */
 export class Data {
 }
+export class TimeSeries extends Data {
 
+  constructor(values, labels) {
+    super();
+    this._values = values;
+    this._labels = labels;
+  }
+}
